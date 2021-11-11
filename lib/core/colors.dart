0e02a15e60093fe97bfeb8523a6cd59b9176fd7f
Julio <jucesargois@gmail.com>
2021-11-11 17:28:28 -0300

    Inserted file for the app colors

---
+++ lib/core/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+abstract class TodoListColors {
+   static const primary = Color(0xFF19227C);
+   static const light = Color(0xFF514AAC);
+   static const dark = Color(0xFF00004F);
+
+
+}

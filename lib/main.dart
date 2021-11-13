720dda094bcf0f7383b0736614bd917545568d77
Julio <jucesargois@gmail.com>
2021-11-12 21:13:20 -0300

    Added splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-import 'screens/splash/splash_view.dart';
+import 'package:app_to_do_list/core/colors.dart';
+import 'package:app_to_do_list/core/images.dart';
+import 'package:app_to_do_list/screens/home/home_view.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const TodoListApp());
@@ -14,7 +18,36 @@ class TodoListApp extends StatelessWidget {
     return const MaterialApp(
       title: 'App ToDoList',
       debugShowCheckedModeBanner: false,
-      home: SplashView(),
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode;
+    Future.delayed(Duration(seconds: 2)).then((_){
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>HomeView()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: TodoListColors.primary,
+        width: double.maxFinite,
+        height: double.maxFinite,
+        child: Image.asset(TodoListImages.logotodolist,scale: 5,)
+      ),
     );
   }
 }

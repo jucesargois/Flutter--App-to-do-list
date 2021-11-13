c03d6a36be1458d1b78922569d3101a9de59cc93
Julio <jucesargois@gmail.com>
2021-11-12 23:38:23 -0300

    Inserted structure

--- lib/screens/home/home_view.dart
+++ lib/screens/home/home_view.dart
@@ -1,5 +1,8 @@
 import 'package:app_to_do_list/core/colors.dart';
+import 'package:app_to_do_list/core/images.dart';
 import 'package:flutter/material.dart';
+
+import 'widgets/card.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({Key? key}) : super(key: key);
@@ -11,10 +14,100 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.maxFinite,
-      height: double.maxFinite,
-      color: TodoListColors.primary,
+    return Scaffold(
+      body: Container(
+        width: double.maxFinite,
+        height: double.maxFinite,
+        color: TodoListColors.primary,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                width: 360,
+                height: 550,
+                color: TodoListColors.primary,
+                child: ListView.builder(
+                  physics: BouncingScrollPhysics(), //desabilita efeito glow
+                  itemCount: 20,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Padding(
+                      padding: const EdgeInsets.only(top: 10.0, left: 5),
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Align(
+                              alignment: Alignment.centerLeft,
+                              child: CardWidget()),
+                          SizedBox(
+                            height: 20,
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                    gradient: LinearGradient(
+                      begin: Alignment(0, 1.5),
+                      end: Alignment(0.5, 0.5),
+                      colors: [
+                        const Color(0xFF19227C),
+                        const Color(0xFF514AAC),
+                      ],
+                    ),
+                  ),
+                  child: Material(
+                    color: Colors.transparent,
+                    child: InkWell(
+                      onTap: () {},
+                      splashColor: Colors.white,
+                      child: Container(
+                        width: 200,
+                        height: 44,
+                        child: Center(
+                          child: RichText(
+                            text: TextSpan(
+                              children: [
+                                WidgetSpan(
+                                  child: Image.asset(
+                                    TodoListImages.addtask,
+                                    scale: 25,
+                                  ),
+                                ),
+                                TextSpan(
+                                  text: "        ",
+                                ),
+                                TextSpan(
+                                  text: 'Adicionar Tarefa',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }

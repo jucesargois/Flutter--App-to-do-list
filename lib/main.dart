1e9fbb1b815e46b996f11bd47388434fe637938d
Julio <jucesargois@gmail.com>
2021-12-30 19:43:10 -0300

    Update main.dart
    Removed comentaries

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 
-//Terá a listagem de tarefas.
-//E um botão para adicionar nova tarefa.
 void main() {
   runApp(const TodoListApp());
 }
